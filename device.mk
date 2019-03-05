b5a838342438306e5e778e8138875e79e82f1aad
neelp19 <37125368+neelp19@users.noreply.github.com>
2019-03-04 22:24:26 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, vendor/xiaomi/lithium/lithium-vendor.mk)
 
 # Vendor properties
-include $(LOCAL_PATH)/vendor_prop.mk
+-include $(LOCAL_PATH)/vendor_prop.mk
 
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -50,9 +50,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
 # NFC
-#PRODUCT_COPY_FILES += \
- #   $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-  #  $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+   $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # TFA calibration
 PRODUCT_PACKAGES += \
